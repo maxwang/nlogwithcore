2c64cb6c69ee0e789ffffc7754e76fcc5e430152
Max Wang <mwang@1080agile.com>
2017-08-03 10:32:50 +1000

    init

---
+++ NLogExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace NLogExample
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+           
+            Logger.Info("test");
+            Console.WriteLine("this is atest");
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ NlogConfigByServices/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace NlogConfigByServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            var provider = services.BuildServiceProvider();
+
+            var factory = provider.GetService<ILoggerFactory>();
+            factory.AddNLog();
+            factory.ConfigureNLog("Nlog.config");
+
+            var logger = provider.GetService<ILogger<Program>>();
+            logger.LogCritical("hello nlog");
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
